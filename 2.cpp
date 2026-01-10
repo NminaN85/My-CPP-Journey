aa46c14b15b580d442cb07b1ed13d3ce4923a1ce
Mina <boles.minan@gmail.com>
2026-01-11 00:43:24 +0100

    Add program to sum three numbers

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   
+	int Num1;
+	int Num2;
+	int Num3;
+
+	cout << "Please enter First number: ";
+	cin >> Num1;
+	cout << "Please enter the Second Number : ";
+	cin >> Num2;
+	cout<<"Please enter Third Number ";
+	cin >> Num3;
+
+	cout << " The Sum Of The Three Nubers Is : " << Num1 + Num2 + Num3;
+
+}
