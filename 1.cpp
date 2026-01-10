8acd063f860a2ecb19aa72f27a8f1d577acaf046
Mina <boles.minan@gmail.com>
2026-01-10 19:15:47 +0100

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   
+	int Num;
+
+	cout << "Please Enter A Number : "<<endl;
+	cin >> Num;
+
+	cout << " Half of "<<Num << " Is  "  << Num/2<< endl;
+
+}
