54faa9ab939c64f744155f45f9468ce11dca2455
Mina <boles.minan@gmail.com>
2026-01-11 12:22:30 +0100

    Create the Average of entered Marks

---
+++ the Average of entered Marks.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   
+	int Mark1;
+	int Mark2;
+	int Mark3;
+
+	cout << "Please enter First Mark : ";
+	cin >> Mark1;
+	cout << "Please enter Second Mark : ";
+	cin >> Mark2;
+	cout << "Please enter Third mark : ";
+	cin >> Mark3;
+
+	cout << "the Average of entered Marks is  " << (Mark1 + Mark2 + Mark3) / 3 << endl;
+
+
+}
