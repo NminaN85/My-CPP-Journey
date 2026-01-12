4f6749422bf26efe449efebdb5bba76f139b0dc9
Mina <boles.minan@gmail.com>
2026-01-12 18:27:58 +0100

    Refactor swap function and improve output messages
    Refactor swap function and enhance user prompts

--- swapnumbersbyfunction.cpp
+++ swapnumbersbyfunction.cpp
@@ -3,32 +3,26 @@
 #include <string>
 using namespace std;
 
-
-
-void SwapNumbersFunctionin(int &Num1,int &Num2)
-
-{	
-
+void SwapNumbersFunctionin(int& Num1, int& Num2)
+{
 	int TempNum;
-
-	TempNum= Num1;
+	TempNum = Num1;
 	Num1 = Num2;
 	Num2 = TempNum;
-	
-
 }
 
 int main() {
 	int Num1;
 	int Num2;
-
+	cout << "Please enter Num1 " << endl;
 	cin >> Num1;
+	cout << "Please enter Num2 " << endl;
 	cin >> Num2;
 
-	SwapNumbersFunctionin(Num1, Num2); 
-		cout << Num1 << " " << Num2;
 
-		
-		//cout << "After swapping: " << Num1 << " " << Num2 << endl;
-}		
+	cout << "Before swapping: Num1 = " << Num1 << " Num2 = " << Num2 << endl;
+	
+	SwapNumbersFunctionin(Num1, Num2);
+	cout << "After swapping: Num1 =  " << Num1 << " Num2 = " << Num2 << endl;
+}
 
