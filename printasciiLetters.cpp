dcf6c583757e711e3ce54ab4132c73efcb3174a3
Mina <boles.minan@gmail.com>
2026-01-13 22:11:49 +0100

    Add program to print ASCII letters A-Z

---
+++ printasciiLetters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    for (int ASCII = 64; ASCII <= 90; ASCII +=1) {
+       
+        cout << char(ASCII) << endl;
+    }
+}
